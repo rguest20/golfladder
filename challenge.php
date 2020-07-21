610c7f2d4257e1ec4d358a03719401637a59f500
rguest20 <67912282+rguest20@users.noreply.github.com>
2020-07-21 20:18:46 +0100

    Page refresh on submit and full submit button becomes available

--- challenge.php
+++ challenge.php
@@ -1,4 +1,11 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+  $name = $_POST["yourname"];
+  $opponentname = $_POST["opponent"];
+} else {
+  $opponentname = "bluebanana";
+  echo "I ran this!";
+}
 $servername = "localhost";
 $username = "root";
 $password = "root";
@@ -7,6 +14,9 @@ $dbname = "golfladder";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 $sql="SELECT * FROM ladder";
+$ladder = mysqli_query($conn, $sql);
+$results = $ladder -> fetch_all(MYSQLI_ASSOC);
+mysqli_close ($conn);
 ?>
 
 
@@ -32,12 +42,6 @@ $sql="SELECT * FROM ladder";
     <title>Title</title>
   </head>
   <body>
-    <?php
-    $ladder = mysqli_query($conn, $sql);
-    $results = $ladder -> fetch_all(MYSQLI_ASSOC);
-    mysqli_close ($conn);
-    print json_encode($results[0]['Name']);
-     ?>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="index.html">Aqualate Golf Ladder 2020</a>
       <button
@@ -92,31 +96,31 @@ $sql="SELECT * FROM ladder";
     <section id="challenge-form">
     <h2>Challenge Page</h2>
     <p>Please fill out the form below to initiate challenge</p>
-        <form action="challenges.php" method = "post">
+        <form action="challenge.php" method = "post">
         <label for = "yourname">Your Name</label>
         <select name="yourname" id="yourname">
             <option value="null">Please select an option</option>
-            <?php
-            $output ="";
-            foreach($results as $x=>$y){
-              $output=$y['Name'];
-              echo '<option value="' . $output . '">' . $output . '</option>';
-            }
-            ?>
+              <?php
+              $output ="";
+              foreach($results as $x=>$y){
+                $output=$y['Name'];
+                echo '<option value="' . $output . '">' . $output . '</option>';
+              }
+              ?>
             </select>
+            <button type="button">Submit Name</button>
         <br>
         <small>Challenges can only be made against people who are not in challenges up to 3 positions ahead of you in the ladder</small><br>
         <label for = "opponent">Who do you challenge?</label>
-        <select name="opponent" id="opponent" disabled>
-        <script>
-
-        const disabler = document.getElementById("yourname");
-        const opponent =document.getElementById("opponent");
-         disabler.addEventListener("yourname", function(){
-          opponent.disabled = (disabler.value === 'Please select an option');
-        })
-        </script>
-        <option value="null">Pleae select an option</option>
+          <?php
+          echo "i ran this too";
+           if ($opponentname == "bluebanana"){
+            echo '<select name="opponent" id="opponent" disabled>';
+          } else {
+            echo '<select name="opponent" id="opponent" >';
+          }
+        ?>
+          <option value="null">Pleae select an option</option>
         <input type = "submit" value="Challenge">
         </form>
         </section>
